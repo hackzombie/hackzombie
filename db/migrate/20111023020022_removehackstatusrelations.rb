ed35adf3b5b616e6704874ad4042030f1b7430a1
Zac Bowling <zac@zacbowling.com>
2011-10-22 19:29:28 -0700

    remark this out for now

--- db/migrate/20111023020022_removehackstatusrelations.rb
+++ db/migrate/20111023020022_removehackstatusrelations.rb
@@ -1,10 +1,10 @@
 class Removehackstatusrelations < ActiveRecord::Migration
   def up
     drop_table :statushackrelations
-    add_column :hacks, :status_id, :integer
+    #add_column :hacks, :status_id, :integer
   end
 
   def down
-    remove_column :hacks, :status_id
+    #remove_column :hacks, :status_id
   end
 end
