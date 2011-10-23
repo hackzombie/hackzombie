426b4dc4ccc8dcb65d8f087273c25c39e59d1c0d
dirkdk <dirk@d17.nl>
2011-10-22 19:06:35 -0700

    status one to many to hacks

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -2,8 +2,8 @@ class Hack < ActiveRecord::Base
   belongs_to :status
   belongs_to :event
   
-  has_many :statushackrelations
-  has_many :statuses, :through => :statushackrelations 
+ # has_many :statushackrelations
+#  has_many :statuses, :through => :statushackrelations 
   
   has_many :hackplatformrelations
   has_many :platforms, :through => :hackplatformrelations
